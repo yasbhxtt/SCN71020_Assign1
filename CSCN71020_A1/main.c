048fed931bb6221e983a51ea672b82cbdfc0c711
Yash <yash.b@live.ca>
2024-10-01 22:45:14 -0400

    Commit #3 - Fixed subtraction function

--- CSCN71020_A1/main.c
+++ CSCN71020_A1/main.c
@@ -3,23 +3,26 @@
 void printWelcomeMenu();
 void printOptions();
 void add();
-
+void subtract();
 
 void main() {
 
-	printWelcomeMenu();
-
-	printOptions();
-
 	int inputNum;
 
+	printWelcomeMenu();
+	printOptions();
+
 	printf("Enter operation number: ");
-	scanf_s("%1o", &inputNum);
+	scanf_s("%d", &inputNum);
 
 	switch (inputNum)
 	{
 	case 1:
 		add();
+		break;
+	case 2:
+		subtract();
+		break;
 	}
 
 }
@@ -44,4 +47,14 @@ void add() {
 	scanf_s("%lf", &num2);
 	result = num1 + num2;
 	printf("%lf + %lf = %lf\n", num1, num2, result);
+}
+
+void subtract() {
+	double num1, num2, result;
+	printf("Enter the first value:");
+	scanf_s("%lf", &num1);
+	printf("Enter the second value:");
+	scanf_s("%lf", &num2);
+	result = num1 - num2;
+	printf("%lf - %lf = %lf\n", num1, num2, result);
 }
